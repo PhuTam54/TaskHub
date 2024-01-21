97cb6ee3cec28ea8ed2ca082e6670d8f5f7e093b
PhuTam54 <phutamytb@gmail.com>
2024-01-22 02:34:58 +0700

    Add complex database + Read related data

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using TaskHub.Data;
+using TaskHub.Models.WorkSpaceViewModels;
+
+namespace TaskHub.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly TaskHubContext _context;
+
+        public UsersController(TaskHubContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index(int? id, int? WorkSpaceIdForMember, int? WorkSpaceIdForDetails)
+        {
+            var viewModel = new UserIndexData();
+            viewModel.Users = await _context.User
+                .Include(i => i.WorkSpaces)
+                    .ThenInclude(i => i.WorkSpaceMembers)
+                        .ThenInclude(i => i.User)
+                .Include(i => i.WorkSpaces)
+                    .ThenInclude(i => i.Boards)
+                        .ThenInclude(i => i.Lists)
+                            .ThenInclude(i => i.TaskItems)
+                                .ThenInclude(i => i.Comments)
+                .AsNoTracking()
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                ViewData["UserID"] = id.Value;
+                User user = viewModel.Users.Where(
+                    i => i.ID == id.Value).Single();
+                viewModel.WorkSpaces = user.WorkSpaces;
+            }
+
+            if (WorkSpaceIdForMember != null)
+            {
+                ViewData["WorkSpaceIdForMember"] = WorkSpaceIdForMember.Value;
+                viewModel.WorkSpaceMembers = viewModel.WorkSpaces.Where(
+                    w => w.WorkSpaceId == WorkSpaceIdForMember).Single().WorkSpaceMembers;
+            }
+
+            if (WorkSpaceIdForDetails != null)
+            {
+                ViewData["WorkSpaceIdForDetails"] = WorkSpaceIdForDetails.Value;
+                viewModel.Boards = viewModel.WorkSpaces.Where(
+                    w => w.WorkSpaceId == WorkSpaceIdForDetails).Single().Boards;
+            }
+
+            return View(viewModel);
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,UserName,Email,Password,LastName,FirstMidName,EnrollmentDate")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,UserName,Email,Password,LastName,FirstMidName,EnrollmentDate")] User user)
+        {
+            if (id != user.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.User == null)
+            {
+                return Problem("Entity set 'TaskHubContext.User'  is null.");
+            }
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+          return (_context.User?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/WorkSpacesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskHub.Data;
+using TaskHub.Models;
+
+namespace TaskHub.Controllers
+{
+    public class WorkSpacesController : Controller
+    {
+        private readonly TaskHubContext _context;
+
+        public WorkSpacesController(TaskHubContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WorkSpaces
+        public async Task<IActionResult> Index()
+        {
+            var workSpaces = _context.WorkSpace
+                .Include(w => w.User)
+                .AsNoTracking();
+            return View(await workSpaces.ToListAsync());
+        }
+
+        // GET: WorkSpaces/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.WorkSpace == null)
+            {
+                return NotFound();
+            }
+
+            var workSpace = await _context.WorkSpace
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.WorkSpaceId == id);
+            if (workSpace == null)
+            {
+                return NotFound();
+            }
+
+            return View(workSpace);
+        }
+
+        // GET: WorkSpaces/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.User, "ID", "ID");
+            return View();
+        }
+
+        // POST: WorkSpaces/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WorkSpaceId,WorkSpaceTitle,WorkSpaceDescription,Status,UserId")] WorkSpace workSpace)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(workSpace);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "ID", "ID", workSpace.UserId);
+            return View(workSpace);
+        }
+
+        // GET: WorkSpaces/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.WorkSpace == null)
+            {
+                return NotFound();
+            }
+
+            var workSpace = await _context.WorkSpace.FindAsync(id);
+            if (workSpace == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "ID", "ID", workSpace.UserId);
+            return View(workSpace);
+        }
+
+        // POST: WorkSpaces/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WorkSpaceId,WorkSpaceTitle,WorkSpaceDescription,Status,UserId")] WorkSpace workSpace)
+        {
+            if (id != workSpace.WorkSpaceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workSpace);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkSpaceExists(workSpace.WorkSpaceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "ID", "ID", workSpace.UserId);
+            return View(workSpace);
+        }
+
+        // GET: WorkSpaces/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.WorkSpace == null)
+            {
+                return NotFound();
+            }
+
+            var workSpace = await _context.WorkSpace
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.WorkSpaceId == id);
+            if (workSpace == null)
+            {
+                return NotFound();
+            }
+
+            return View(workSpace);
+        }
+
+        // POST: WorkSpaces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.WorkSpace == null)
+            {
+                return Problem("Entity set 'TaskHubContext.WorkSpace'  is null.");
+            }
+            var workSpace = await _context.WorkSpace.FindAsync(id);
+            if (workSpace != null)
+            {
+                _context.WorkSpace.Remove(workSpace);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkSpaceExists(int id)
+        {
+          return (_context.WorkSpace?.Any(e => e.WorkSpaceId == id)).GetValueOrDefault();
+        }
+    }
+}
